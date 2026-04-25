633f17dc062de7ef5d49ac82095a91153800bfdf
jiteshjunwal <jiteshjunwal@gmail.com>
2026-04-26 02:14:56 +0530

    initial agent setup

---
+++ run_agent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "SCRIPT STARTED"
+
+mkdir -p output
+
+echo "Reading prompt..."
+PROMPT=$(cat prompt.txt)
+
+echo "Calling Groq..."
+
+curl https://api.groq.com/openai/v1/chat/completions \
+  -H "Authorization: Bearer $GROQ_API_KEY" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"model\": \"llama-3.1-8b-instant\",
+    \"messages\": [
+      {\"role\": \"user\", \"content\": \"$PROMPT\"}
+    ]
+  }" > output/result.json
+
+echo "Saving output..."
+
+jq -r '.choices[0].message.content' output/result.json > output/generated_code.txt
+
+echo "Done!"
